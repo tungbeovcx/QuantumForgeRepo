f477a9a79aace1ddc2809f6d00a285eff3140001
tungbeovcx <asdas32432pun0suy@hotmail.com>
2024-05-11 01:25:04 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-function pathSum(root, sum) {
-  if (!root) return [];
-  const result = [];
-  const path = [];
-  const traverse = (node, sum) => {
-    if (!node) return;
-    path.push(node.val);
-    if (!node.left && !node.right && sum === node.val) {
-      result.push([...path]);
-    }
-    traverse(node.left, sum - node.val);
-    traverse(node.right, sum - node.val);
-    path.pop();
-  };
-  traverse(root, sum);
-  return result;
-}
+const isEven = (num) => num % 2 === 0;
