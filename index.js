c1d76106f04d262226d37f350e6b1699cbf7c9fc
tungbeovcx <asdas32432pun0suy@hotmail.com>
2024-05-15 21:45:26 +0700

    test project commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const lcm = (a, b) => {
-  return (a * b) / gcd(a, b);
-};
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
